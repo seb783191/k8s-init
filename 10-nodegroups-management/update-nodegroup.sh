b3062a46ec53b43b80501c9b0f00068b756a5e26
seb-cloud9 <ec2-user@ip-172-31-4-89.ec2.internal>
2022-05-10 09:12:12 +0000

    up

--- 10-nodegroups-management/update-nodegroup.sh
+++ 10-nodegroups-management/update-nodegroup.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+if [ $# -lt 1 ]; then
+	  echo "Argument manquant: <Environnement:[cdn1|cdn2|...]>"
+	    exit 1
+fi
+
+ENV=$1
 eksctl upgrade nodegroup \
-	  --name=nodegroup \
+	  --name=nodegroup1 \
 	    --cluster=$ENV-eksctl \
-	      --kubernetes-version=1.20
+	      --kubernetes-version=1.21
