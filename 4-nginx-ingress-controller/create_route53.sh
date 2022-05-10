7e310e82726be2d37abbdd767b2a21ccf9be0ea7
seb-cloud9 <ec2-user@ip-172-31-4-89.ec2.internal>
2022-05-10 12:04:22 +0000

    up

--- 4-nginx-ingress-controller/create_route53.sh
+++ 4-nginx-ingress-controller/create_route53.sh
@@ -3,4 +3,4 @@ HostedZoneName="$2"
 Name="*.$ENV.$HostedZoneName"
 AlbName=$3
 HostedZoneId=$4
-aws cloudformation deploy --template-file route53.yaml  --stack-name eksctl-cdn1-eksctl-route53 --capabilities CAPABILITY_NAMED_IAM --parameter-overrides HostedZoneName=$HostedZoneName"." Name=$Name AlbName=$AlbName HostedZoneId=$HostedZoneId
+aws cloudformation deploy --template-file route53.yaml  --stack-name eksctl-$ENV-eksctl-route53 --capabilities CAPABILITY_NAMED_IAM --parameter-overrides HostedZoneName=$HostedZoneName"." Name=$Name AlbName=$AlbName HostedZoneId=$HostedZoneId
---
+++ 5-console-okd/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -lt 3 ]; then
+  echo "Argument manquant: <url_console_voulue> <username_console> <password>"
+  exit 1
+fi
+url=$1
+username=$2
+password=$3
+htpasswd -bc auth_file $2 $3
+encoded_auth=$(cat auth_file|base64)
+values_file="values_final.yaml"
+
+echo -e "host: $url\nauth: $encoded_auth" > $values_file
+
+cat $values_file
+helm upgrade --install origin-console -f $values_file . -norigin-console --create-namespace
+rm auth_file $values_file
