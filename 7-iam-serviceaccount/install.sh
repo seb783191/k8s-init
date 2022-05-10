ce251681b0fde903b9b0f7cd6a67a6830f6914cb
seb-cloud9 <ec2-user@ip-172-31-4-89.ec2.internal>
2022-05-10 12:55:49 +0000

    up

--- 7-iam-serviceaccount/install.sh
+++ 7-iam-serviceaccount/install.sh
@@ -7,14 +7,20 @@ if [ $# -lt 1 ]; then
 fi
 
 ENV=$1
+NAMESPACE=bcloud-operator-system
+SERVICE_ACCOUNT_NAME=bcloud-operator-controller-manager
+file=trust-$ENV.json
+
 export ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 export OIDC_PROVIDER=$(aws eks describe-cluster --name $ENV-eksctl --query "cluster.identity.oidc.issuer" --output text | sed -e "s/^https:\/\///")
 export OIDC_ID=$(aws eks describe-cluster --name $ENV-eksctl --query "cluster.identity.oidc.issuer" --output text |awk -F "/" '{print $5}')
 
-aws iam list-open-id-connect-providers | grep ${OIDC_ID}
+#aws iam list-open-id-connect-providers | grep ${OIDC_ID}
+
+#sed
+sed -e 's|ACCOUNT_ID|'$ACCOUNT_ID'|; s|OIDC_PROVIDER|'$OIDC_PROVIDER'|; s|OIDC_ID|'$OIDC_ID'|; s|NAMESPACE|'$NAMESPACE'|; s|SERVICE_ACCOUNT_NAME|'$SERVICE_ACCOUNT_NAME'|' trust.json > $file
 
 eksctl utils associate-iam-oidc-provider --cluster $ENV-eksctl --approve
-
-aws iam create-role --role-name $ENV-eksctl-bcloud-operator --assume-role-policy-document file://trust.json --description "role for bcloud"
-
+aws iam create-role --role-name $ENV-eksctl-bcloud-operator --assume-role-policy-document file://$file --description "role for bcloud"
 aws iam attach-role-policy --role-name $ENV-eksctl-bcloud-operator --policy-arn=arn:aws:iam::aws:policy/AdministratorAccess
+rm $file
