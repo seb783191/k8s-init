c2c945956b8d434dc48359c32244c89b902da2bc
seb <sbdejou@example.com>
2022-07-25 16:59:13 +0200

    remove grafana repo

--- 9-prometheus/install.sh
+++ 9-prometheus/install.sh
@@ -3,9 +3,6 @@
 # add prometheus Helm repo
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 
-# add grafana Helm repo
-helm repo add grafana https://grafana.github.io/helm-charts
-
 
 kubectl create namespace prometheus
 
